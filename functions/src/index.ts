757aec6a7513e7a1cff3f6ee02c8d3e93003ce4c
ryuhangyeong <hangyeong.ryu@gmail.com>
2021-02-09 00:19:18 +0900

    [추가] 카카오 로그인, getUserProfile 및 필요 라이브러리 설치

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,43 @@
 import * as functions from 'firebase-functions';
 import * as cors from 'cors';
+import axios from 'axios';
 
+/*
+ * @todo origin askyou만 접근하도록 변경 예정
+ */
 const crossOrigin = cors({
   origin: true,
 });
 
-export const helloWorld = functions.https.onRequest(async (_, res) => {
-  res.send('Hello from Firebase!');
-});
+const getUserProfile = async (accessToken: string) => {
+  const url: string = 'https://kapi.kakao.com/v2/user/me?secure_resource=true';
 
-export const OauthKakao = functions.https.onRequest(async (_, res) => {
-  return crossOrigin(_, res, () => {
-    res.json({ hello: 'world' });
+  const data = await axios({
+    url,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return data;
+};
+
+export const OauthKakao = functions.https.onRequest(async (req, res) => {
+  if (req.method === 'POST') {
+    const { accessToken } = req.body;
+
+    try {
+      const data = await getUserProfile(accessToken);
+
+      return crossOrigin(req, res, () => {
+        res.json({ hello: 'world' });
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return crossOrigin(req, res, () => {
+    res.json({});
   });
 });
