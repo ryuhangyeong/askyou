14cf98cfa165a5446ad33b4c7b2721bed9c907b1
ryuhangyeong <hangyeong.ryu@gmail.com>
2021-02-14 15:44:19 +0900

    [추가] Introduce, mbti list 슬라이드 효과 구현

--- src/components/Introduce/Introduce.tsx
+++ src/components/Introduce/Introduce.tsx
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import Responsive from '../Responsive';
+import { getMBTIs } from '../../mbti/getMBTIs';
 
 export default () => {
+  const SIZE = 65;
+  const DURATION = 2000;
+  const [idx, setIdx] = useState(1);
+  const ulElement = useRef<HTMLUListElement>(null);
+
+  const mbtis = getMBTIs.concat({
+    ...getMBTIs[0],
+    id: getMBTIs[getMBTIs.length - 1].id + 1,
+  });
+
+  const len = mbtis.length;
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const { current } = ulElement;
+
+      if (current !== null) {
+        setIdx(idx + 1);
+        current.style.transition = `all ${DURATION / 10000}s ease-in-out`;
+        current.style.transform = `translateY(-${SIZE * idx}px)`;
+        if (idx >= len - 1) {
+          setTimeout(() => {
+            current.style.transition = 'none';
+            current.style.transform = 'none';
+            setIdx(1);
+          }, DURATION / 10);
+        }
+      }
+    }, DURATION);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [idx, len]);
   return (
     <Wrapper>
       <Inner>
-        <h2>여러분이</h2>
-        <h2>
-          <span>INFP</span> 에게
+        <h2 className="slide">
+          <ul ref={ulElement}>
+            {mbtis.map((mbti) => (
+              <li key={mbti.id}>
+                <span>{mbti.type}</span>
+              </li>
+            ))}
+          </ul>
         </h2>
-        <h2>궁금한 모든 것을 물어보세요.</h2>
+        <span>에게</span>
+        <h2>궁금한 모든 것을 질문하세요.</h2>
       </Inner>
     </Wrapper>
   );
@@ -20,6 +61,7 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  overflow: hidden;
   width: 100%;
   height: 500px;
   background-color: #e9ecef;
@@ -36,12 +78,38 @@ const Inner = styled(Responsive)`
     font-size: 5rem;
     font-weight: 300;
 
-    span {
+    &.slide {
+      position: relative;
+      overflow: hidden;
       display: inline-block;
-      padding: 0 10px;
-      background-color: #000;
-      color: #fff;
-      font-weight: 700;
+      width: 130px;
+      height: 65px;
+      line-height: 65px;
+
+      ul {
+        position: absolute;
+        width: 100%;
+        top: 0;
+        transition: all 0.2s ease-in-out;
+        li {
+          text-align: center;
+          height: 65px;
+
+          span {
+            display: block;
+            height: inherit;
+            background-color: #000;
+            color: #fff;
+            font-weight: 700;
+          }
+        }
+      }
     }
+  }
+  span {
+    vertical-align: top;
+    line-height: 65px;
+    font-size: 5rem;
+    font-weight: 300;
   }
 `;
---
+++ src/mbti/getMBTIs.ts
@@ -0,0 +1,104 @@
+type MBTIType = {
+  id: number;
+  type: string;
+  alias: string;
+};
+
+export const getAnalysisType: MBTIType[] = [
+  {
+    id: 1,
+    type: 'INTJ',
+    alias: '인티제',
+  },
+  {
+    id: 2,
+    type: 'INTP',
+    alias: '인팁',
+  },
+  {
+    id: 3,
+    type: 'ENTJ',
+    alias: '엔티제',
+  },
+  {
+    id: 4,
+    type: 'ENTP',
+    alias: '엔팁',
+  },
+];
+
+export const getDiplomacyType: MBTIType[] = [
+  {
+    id: 5,
+    type: 'INFJ',
+    alias: '인프제',
+  },
+  {
+    id: 6,
+    type: 'INFP',
+    alias: '인프피',
+  },
+  {
+    id: 7,
+    type: 'ENFJ',
+    alias: '엔프제',
+  },
+  {
+    id: 8,
+    type: 'ENFP',
+    alias: '엔프피',
+  },
+];
+
+export const getManagerType: MBTIType[] = [
+  {
+    id: 9,
+    type: 'ISTJ',
+    alias: '잇티제',
+  },
+  {
+    id: 10,
+    type: 'ISFJ',
+    alias: '잇프제',
+  },
+  {
+    id: 11,
+    type: 'ESTJ',
+    alias: '엣티제',
+  },
+  {
+    id: 12,
+    type: 'ESFJ',
+    alias: '엣프제',
+  },
+];
+
+export const getExplorerType: MBTIType[] = [
+  {
+    id: 13,
+    type: 'ISTP',
+    alias: '잇팁',
+  },
+  {
+    id: 14,
+    type: 'ISFP',
+    alias: '잇프피',
+  },
+  {
+    id: 15,
+    type: 'ESTP',
+    alias: '엣팁',
+  },
+  {
+    id: 16,
+    type: 'ESFP',
+    alias: '엣프피',
+  },
+];
+
+export const getMBTIs: MBTIType[] = [
+  ...getAnalysisType,
+  ...getDiplomacyType,
+  ...getManagerType,
+  ...getExplorerType,
+];
