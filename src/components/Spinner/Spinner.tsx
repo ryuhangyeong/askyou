e0d98374bc0d821b4531a04c19a4b12af49dee44
ryuhangyeong <hangyeong.ryu@gmail.com>
2021-02-20 16:43:35 +0900

    [수정] Spinner 컴포넌트 애니메이션 클래스명 수정

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -41,21 +41,21 @@ const Wrapper = styled.div`
     animation-timing-function: cubic-bezier(0, 1, 1, 0);
     &:nth-child(1) {
       left: 8px;
-      animation: loader1 0.6s infinite;
+      animation: spinner1 0.6s infinite;
     }
     &:nth-child(2) {
       left: 8px;
-      animation: loader2 0.6s infinite;
+      animation: spinner2 0.6s infinite;
     }
     &:nth-child(3) {
       left: 32px;
-      animation: loader2 0.6s infinite;
+      animation: spinner2 0.6s infinite;
     }
     &:nth-child(4) {
       left: 56px;
-      animation: loader3 0.6s infinite;
+      animation: spinner3 0.6s infinite;
     }
-    @keyframes loader1 {
+    @keyframes spinner1 {
       0% {
         transform: scale(0);
       }
@@ -63,7 +63,7 @@ const Wrapper = styled.div`
         transform: scale(1);
       }
     }
-    @keyframes loader2 {
+    @keyframes spinner2 {
       0% {
         transform: translate(0, 0);
       }
@@ -71,7 +71,7 @@ const Wrapper = styled.div`
         transform: translate(24px, 0);
       }
     }
-    @keyframes loader3 {
+    @keyframes spinner3 {
       0% {
         transform: scale(1);
       }
