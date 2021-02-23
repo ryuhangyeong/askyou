f6893a6bc8a0628dd07978d4a9a6e49173c39087
ryuhangyeong <hangyeong.ryu@gmail.com>
2021-02-24 00:54:33 +0900

    [수정] survey hooks & redux 로직 추가

--- src/hooks/useSurvey.ts
+++ src/hooks/useSurvey.ts
@@ -1,18 +1,27 @@
 import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../modules';
-import { surveyAdd } from '../modules/survey';
+import { surveyLoading, surveySelect } from '../modules/survey';
 
 export default function useSurvey() {
-  const { list } = useSelector((state: RootState) => state.survey);
+  const { loading, list, select } = useSelector(
+    (state: RootState) => state.survey
+  );
   const dispatch = useDispatch();
 
-  const onSurveyAdd = useCallback((data) => dispatch(surveyAdd(data)), [
+  const onSurveyLoading = useCallback((data) => dispatch(surveyLoading(data)), [
+    dispatch,
+  ]);
+
+  const onSurveySelect = useCallback((data) => dispatch(surveySelect(data)), [
     dispatch,
   ]);
 
   return {
+    loading,
     list,
-    onSurveyAdd,
+    select,
+    onSurveyLoading,
+    onSurveySelect,
   };
 }
--- src/modules/survey.ts
+++ src/modules/survey.ts
@@ -1,29 +1,45 @@
 import { deprecated, ActionType, createReducer } from 'typesafe-actions';
-import { ISurvey } from '../mbti/getSurvey';
+import { getSurvey, ISurvey } from '../mbti/getSurvey';
 
 const { createStandardAction } = deprecated;
 
-const SURVEY_ADD = 'survey/SURVEY_ADD';
+const SURVEY_LOADING = 'survey/SURVEY_LOADING';
+const SURVEY_SELECT = 'survey/SURVEY_SELECT';
 
-export const surveyAdd = createStandardAction(SURVEY_ADD)<string>();
+export const surveyLoading = createStandardAction(SURVEY_LOADING)<boolean>();
+export const surveySelect = createStandardAction(SURVEY_SELECT)<ISurvey>();
 
-const actions = { surveyAdd };
+const actions = { surveyLoading, surveySelect };
 
 type SurveyAction = ActionType<typeof actions>;
 
 type SurveyState = {
+  loading: boolean;
   list: ISurvey[];
+  select: ISurvey[];
+  mbti: string;
 };
 
 const initialState: SurveyState = {
-  list: [],
+  loading: false,
+  list: getSurvey,
+  select: [],
+  mbti: '',
 };
 
 const survey = createReducer<SurveyState, SurveyAction>(initialState, {
-  [SURVEY_ADD]: (state, action) => ({
+  [SURVEY_LOADING]: (state, action) => ({
     ...state,
-    // data: state.data.concat(action.payload),
+    loading: action.payload,
   }),
+  [SURVEY_SELECT]: (state, action) => {
+    const select = [...state.select, action.payload];
+    // @TODO
+    return {
+      ...state,
+      select,
+    };
+  },
 });
 
 export default survey;
