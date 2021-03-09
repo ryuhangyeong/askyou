9acb11f9cc9b3d77a0c736d5ba750cbb6814019b
ryuhangyeong <hangyeong.ryu@gmail.com>
2021-03-09 23:56:44 +0900

    [추가] chart.js 설치 및 mbti 테스트 결과 가져오기, 바 차트 테스트

--- src/api/survey.ts
+++ src/api/survey.ts
@@ -1,18 +1,23 @@
 import firebase from 'firebase/app';
 import { getAnalysisMbti, ISurveyItem } from '../data/survey';
 
-const datbase = firebase.database().ref('survey/');
+const database = firebase.database().ref('survey/');
 
 export const createSurvey = async (payload: {
   uid: string | undefined;
   select: ISurveyItem[];
 }) => {
   const { uid = '', select = [] } = payload;
-  const data = await datbase.push({
+  const data = await database.push({
     created: firebase.database.ServerValue.TIMESTAMP,
     mbti: getAnalysisMbti(select),
     uid,
     select,
   });
   return data;
+};
+
+export const getSurvey = async () => {
+  const data = await database.once('value');
+  return Object.values(data.val());
 };
--- src/components/Analysis/Analysis.tsx
+++ src/components/Analysis/Analysis.tsx
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { HorizontalBar } from 'react-chartjs-2';
+import Responsive from '../Responsive';
+import { getSurvey } from '../../api/survey';
+
+const options = {
+  legend: {
+    display: false,
+  },
+  scales: {
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+      },
+    ],
+    xAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          display: false,
+        },
+      },
+    ],
+    ticks: {
+      display: false,
+    },
+  },
+};
 
 export default () => {
+  useEffect(() => {
+    getSurvey();
+  }, []);
+
+  const calculatedArr = [10, 20, 5, 5, 10, 2, 7, 1];
+  const rankColor = [
+    '#11b288',
+    '#207ac7',
+    '#207ac7',
+    '#207ac7',
+    '#d6d6d6',
+    '#d6d6d6',
+    '#d6d6d6',
+    '#d6d6d6',
+  ];
+
+  const data = {
+    labels: ['INFP', 'INTP', 'INTJ', 'ISTP', 'ENTP', 'ENFP', 'ISTJ', 'ENTJ'],
+    datasets: [
+      {
+        backgroundColor: rankColor,
+        borderColor: rankColor,
+        borderWidth: 1,
+        hoverBackgroundColor: rankColor,
+        hoverBorderColor: rankColor,
+        data: calculatedArr,
+      },
+    ],
+  };
+
   return (
-    <div>
-      <h1>Analysis</h1>
-    </div>
+    <Layout>
+      <HorizontalBar data={data} options={options} />
+    </Layout>
   );
 };
+
+const Layout = styled(Responsive)`
+  display: flex;
+
+  @media (min-width: 768px) {
+  }
+`;
