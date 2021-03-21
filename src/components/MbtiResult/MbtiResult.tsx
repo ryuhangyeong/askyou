e14f52928389f48a1425eb61edebd9995c49f63c
ryuhangyeong <hangyeong.ryu@gmail.com>
2021-03-21 20:08:17 +0900

    [추가] MBTI 결과 페이지 - text cloud chart

--- src/components/MbtiResult/MbtiResult.tsx
+++ src/components/MbtiResult/MbtiResult.tsx
@@ -1,20 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4pluginsWordCloud from '@amcharts/amcharts4/plugins/wordCloud';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import useSurvey from '../../hooks/useSurvey';
 
 export default () => {
+  const chartRef = useRef<am4pluginsWordCloud.WordCloud | null>(null);
   const { select, mbti } = useSurvey();
   const history = useHistory();
 
   useEffect(() => {
-    if (!mbti) {
-      history.push('/');
-    }
-  }, [mbti, history]);
+    if (!mbti || !select.length) history.push('/');
+
+    const chart = am4core.create('chart', am4pluginsWordCloud.WordCloud);
+    chart.fontFamily = 'NotoSansKR';
+
+    const series = chart.series.push(new am4pluginsWordCloud.WordCloudSeries());
+    series.text = select.map((data) => data.title).join(' ');
+    series.maxFontSize = am4core.percent(100);
+
+    chartRef.current = chart;
+    return () => chart.dispose();
+  }, [history, mbti, select]);
 
   if (!mbti) return null;
-  return <Wrapper>{select.length}</Wrapper>;
+  return (
+    <Layout>
+      <header>
+        <h1>{mbti}</h1>
+      </header>
+      <div id="chart" style={{ width: '100%', height: '100%' }} />
+    </Layout>
+  );
 };
 
-const Wrapper = styled.div``;
+const Layout = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+
+  header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    h1 {
+      text-align: center;
+      font-size: 5rem;
+    }
+  }
+`;
