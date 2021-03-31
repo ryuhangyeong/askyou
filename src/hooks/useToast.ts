8b36b59fe6c5cd26225af9a24cf6ceae2a699402
ryuhangyeong <hangyeong.ryu@gmail.com>
2021-04-01 00:10:40 +0900

    [수정] toast ui (toggle) 로직 hooks에 추가

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -2,17 +2,30 @@ import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../modules';
 import { toastOpen } from '../modules/toast';
+import useAnimation from './useAnimation';
 
 export default function useToast() {
   const { message } = useSelector((state: RootState) => state.toast);
   const dispatch = useDispatch();
+  const { visible, animate, comeback } = useAnimation();
 
   const onToastOpen = useCallback((text: string) => dispatch(toastOpen(text)), [
     dispatch,
   ]);
 
+  const onToast = useCallback(
+    (text: string) => {
+      comeback();
+      onToastOpen(text);
+    },
+    [comeback, onToastOpen]
+  );
+
   return {
+    visible,
+    animate,
     message,
+    onToast,
     onToastOpen,
   };
 }
